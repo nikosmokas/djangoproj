d6876a7d2e3526d1271d5441c091b757e6e43915
Nikolaos Mokas <nikosmokas@hotmail.gr>
2024-04-26 15:51:24 +0300

    Django Project - init commit

---
+++ accounts/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            # Redirect to a success page
+            return redirect('home')
+        else:
+            # Display an error message
+            return render(request, 'login.html', {'error': 'Invalid username or password'})
+    else:
+        return render(request, 'login.html')
